f6b81002989f427d1d62cc589d7f0ea5f527bbeb
parth <parth280197@gmail.com>
2019-06-22 22:07:12 -0500

    gh-pages update

---
+++ build/precache-manifest.eddff2be830f1cb8bfb5742144da40ea.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b539e135651a5069324c2515a190b29",
+    "url": "/book-app/static/media/heart.7b539e13.svg"
+  },
+  {
+    "revision": "d79c60d75712bb35249b",
+    "url": "/book-app/static/css/main.a79d6d4c.chunk.css"
+  },
+  {
+    "revision": "47b1842aff192e3ce5ba",
+    "url": "/book-app/static/js/1.47b1842a.chunk.js"
+  },
+  {
+    "revision": "7e02ea49838fd11623e6",
+    "url": "/book-app/static/js/runtime~main.7e02ea49.js"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/book-app/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "d79c60d75712bb35249b",
+    "url": "/book-app/static/js/main.d79c60d7.chunk.js"
+  },
+  {
+    "revision": "f88f4a8e0cdee4521743c5d644d89293",
+    "url": "/book-app/static/media/done.f88f4a8e.svg"
+  },
+  {
+    "revision": "5128eaf16911369d4822cec990cb96b9",
+    "url": "/book-app/static/media/hourglass.5128eaf1.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/book-app/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/book-app/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "4ce6b70d51f5a312b99b1e0360ded0bb",
+    "url": "/book-app/index.html"
+  }
+];
